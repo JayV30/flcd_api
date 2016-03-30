82f3e482a59c836030cadec76172587b267e2230
Jay Karlsven <j.karlsven@gmail.com>
2016-03-30 22:24:36 +0000

    Move controller logic from app.js to separate controllers

--- app.js
+++ app.js
@@ -1,73 +1,41 @@
 // load packages
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./db.js');
+var db = require('./db.js'); // contains the models & db methods
+var controller = require('./lib/controllers/controllerIndex.js'); // contains references to all controllers
 
 // set some values
 var port = process.env.PORT || 3000;
 
 // create the express application
 var app = express();
+var router = express.Router();
 
 // set up some middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// User endpoints
+router.route('/users')
+      .post(controller.user.create);
+      
+// Deck endpoints
+router.route('/decks')
+      .get(controller.deck.getVisible)
+      .post(controller.deck.create);
+      
+router.route('/decks/:id')
+      .get(controller.deck.getOne)
+      .put(controller.deck.updateOne)
+      .delete(controller.deck.deleteOne);
 
 // proof of life
 app.get('/', function(req, res) {
    res.json({message: "Welcome to the Flash Card API"}); 
 });
 
-// User Routes
-app.post('/users', function(req, res) {
-    db.user.create(req.body).then(function(user) {
-        res.status(201).json(user.toJSON());
-    }, function(e) {
-        res.status(400).json(e);
-    });
-});
-
-// Deck Routes
-app.post('/decks', function(req, res) {
-   db.deck.create(req.body).then(function(deck) {
-       res.status(201).json(deck.toJSON());
-   }, function(e) {
-       res.status(400).json(e);
-   });
-});
-
-app.get('/decks', function(req, res) {
-    db.deck.findAll().then(function(decks) {
-        res.json(decks);
-    }, function(e) {
-        res.status(500).json(e);
-    });
-});
-
-app.get('/decks/:id', function(req, res) {
-   db.deck.findById(req.params.id).then(function(deck) {
-       if (!!deck) res.json(deck);
-       else res.status(404).send();
-   }, function(e) {
-       res.status(500).json(e);
-   }) 
-});
-
-app.put('/decks/:id', function(req, res) {
-    db.deck.findById(req.params.id).then(function(deck) {
-        if (!!deck) {
-            deck.update(req.body).then(function(deck) {
-                res.json(deck);
-            }, function(e) {
-                res.status(400).json(e);
-            });
-        } else {
-            res.status(404).send();
-        }
-    }, function(e) {
-        res.status(500).json(e);
-    });
-});
+// register routes
+app.use("/", router);
 
 // connect to the db and start the app...
 db.sequelize.sync().then(function() {
